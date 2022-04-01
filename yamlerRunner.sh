b76e8f88c560c612fcd4d5d97a1bb95c1eb49c2f
Michael Baudis <mbaudis@me.com>
2022-04-01 10:16:02 +0200

    beaconYamler stub
    ... not yet working

--- yamlerRunner.sh
+++ yamlerRunner.sh
@@ -1,8 +1,12 @@
-BYCONPATH=~/groupbox/dbtools/bycon
-BYCONEERPATH=~/groupbox/dbtools/bycon
-BYCONRSRCPATH=~/groupbox/dbtools/bycon/beaconServer/rsrc
-BEACONMODELPATH=~/GitHub/beacon-v2-Models/BEACON-V2-draft4-Model
-BEACONFRAMEWORKPATH=~/GitHub/beacon-framework-v2
+BASEDIR=$(dirname $0)
+UNITYPATH=$BASEDIR/../bycon/beacon_schemas
 
-./yamler.py -i $BEACONMODELPATH -t json -o $BYCONRSRCPATH/beacon-v2-dev/model
-./yamler.py -i $BEACONFRAMEWORKPATH -t json -o $BYCONRSRCPATH/beacon-v2-dev/framework
+# initial conversion from separate schemas
+BEACONMODELPATH=/Users/$USER/Github/beacon-v2/models
+BEACONFRAMEWORKPATH=/Users/$USER/Github/beacon-v2/framework
+
+for KIND in src json
+do
+	mkdir -p $UNITYPATH/models/$KIND
+	mkdir -p $UNITYPATH/framework/$KIND	
+done
